3e4447a5839785460f39770aa07b8c196859b211
David-B-A <dabelloa@unal.edu.co>
2021-03-08 23:40:42 -0500

    Feature/making provisional dashboard ¡¡warning: selectRaw used in home controller!!

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Sale;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $salesPerMonth = Sale::selectRaw(
+            'CONCAT(year(updated_at),"-",IF(month(updated_at)>=10,month(updated_at),CONCAT("0",month(updated_at)))) as date_str, sum(total) as sales'
+        )
+        ->where('payment_status','Pagado')
+        ->groupBy('date_str')
+        ->orderBy('date_str')
+        ->get();
+        return view('home',compact('salesPerMonth'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,99 @@
+@push('page_css')
+<style>
+.chart{
+    width: 100%; 
+    min-height: 450px;
+}
+</style>
+@endpush
+
+
 @extends('layouts.app')
 
+
 @section('content')
-<div class="container">
-    <div class="row">
 
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Dashboard</h1>
+                </div>
+            </div>
+        </div>
+    </section>
 
+    <div class="content px-3">
+
+        @include('flash::message')
+
+        <div class="clearfix"></div>
+
+        <div class="card">
+            <div class="card-body p-2">
+    
+                <div id="chart_lines" class="chart"></div>
+
+                <div class="card-footer clearfix float-right">
+                    <div class="float-right">
+                        
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
-</div>
 @endsection
+
+@push('page_scripts')
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.setOnLoadCallback(drawLinesChart);
+var salesPerMonth={!! json_encode($salesPerMonth) !!};
+function drawLinesChart() {
+    console.log(salesPerMonth)
+    var data = new google.visualization.DataTable();
+      data.addColumn('date', 'X');
+      data.addColumn('number', 'Ventas Totales');
+    for(i = 0; i < salesPerMonth.length;i++){
+        date = salesPerMonth[i].date_str;
+        if(date){
+          date = date.split('-');
+          data.addRow( [new Date(date[0], (parseInt(date[1]) - 1).toString() ),  salesPerMonth[i].sales]);
+        }   
+    }
+    startDate = salesPerMonth[0].date_str;
+    startDate = startDate.split('-');
+    endDate = salesPerMonth[salesPerMonth.length - 1].date_str;
+    endDate = endDate.split('-');
+    difannios = (endDate[0]-startDate[0])
+    difmeses = (endDate[1]-startDate[1]);
+    var monthLines = [];
+    for(i = 0; i<=difannios; i++){
+        for(j = 0;j<=difmeses;j++){
+            monthLines.push(new Date((parseInt(startDate[0]) + i).toString(),(parseInt(startDate[1]) + j - 1).toString()));
+        }
+    }
+      var options = {
+        title: 'Ventas totales por Mes',
+        hAxis: {
+            title: 'Mes',
+            format: 'MMM-yy',
+            ticks: monthLines
+        },
+        vAxis: {
+          title: 'Ventas'
+        },
+        series: {
+          1: {curveType: 'function'}
+        },
+        // trendlines: {
+        //   0: {type: 'linear', color: '#111', opacity: .3}
+        // }
+      };
+      var chart = new google.visualization.LineChart(document.getElementById('chart_lines'));
+      chart.draw(data, options);
+}
+</script>
+@endpush
